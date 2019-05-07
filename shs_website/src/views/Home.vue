13d3b8030d005325526b3cbf15c4d088ae0e3fe9
monroy95 <m.monroyc22@gmail.com>
2019-05-06 23:05:47 -0600

    Practica con vuetify textos-colores

---
+++ shs_website/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Home">
+    <h1>Inicio</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae accusamus at esse aliquid atque assumenda nobis, magnam nesciunt eligendi ullam repudiandae, saepe quo vero tempore dolore ut aspernatur impedit quibusdam?</p>
+
+    <p
+      class="pink lighten-4 red--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel magni obcaecati accusantium deserunt debitis numquam eum amet similique commodi ex eius enim veritatis eos at maxime, nam ea culpa dolorem.</p>
+
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
