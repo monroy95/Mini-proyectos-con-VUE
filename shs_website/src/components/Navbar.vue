9e893c94a2e74b4873607902cfba95c13e37b361
monroy95 <m.monroyc22@gmail.com>
2019-05-12 19:39:19 -0600

    toolbars

--- shs_website/src/components/Navbar.vue
+++ shs_website/src/components/Navbar.vue
@@ -17,7 +17,18 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="indigo">
-      <p>test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <!-- Agrega el icono -->
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <!-- Agrega el contenido -->
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -26,7 +37,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
     };
   }
 };
